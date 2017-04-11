5e2ac595d747bbfda929bc5dda2f2b4d5af97b62
Sebastian Kaiser <sebastian.kaiser@capgemini.com>
2017-04-11 10:52:23 +0200

    Restructure sample project

---
+++ simple-jar/src/test/java/HelloTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by seka on 27.06.16.
+ */
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloTest {
+    @Test
+    public void testPerformTest() {
+        Assert.assertEquals(0, 0);
+    }
+}
